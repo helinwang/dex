dbe218f8f42dd05fd37437e4da10a197e26b6577
Helin Wang <ustc.harry@gmail.com>
2018-05-18 14:20:22 -0700

    move system state in node into chain

--- pkg/consensus/node.go
+++ pkg/consensus/node.go
@@ -1,9 +1,6 @@
 package consensus
 
 import (
-	"bytes"
-	"encoding/gob"
-	"errors"
 	"log"
 
 	"github.com/dfinity/go-dfinity-crypto/bls"
@@ -14,18 +11,20 @@ import (
 // Nodes form a group randomly, the randomness comes from the random
 // beacon.
 type Node struct {
-	addr       Addr
-	sk         bls.SecretKey
-	net        *Networking
-	roundInfo  *RoundInfo
-	nodeIDToPK map[int]bls.PublicKey
-	addrToPK   map[Addr]bls.PublicKey
-	idToGroup  map[int]*Group
+	addr      Addr
+	sk        bls.SecretKey
+	net       *Networking
+	roundInfo *RoundInfo
 
 	// the memberships of different groups
 	memberships map[bls.PublicKey]membership
 	chain       *Chain
 	pendingTxns [][]byte
+}
+
+type membership struct {
+	skShare bls.SecretKey
+	g       *Group
 }
 
 // NewNode creates a new node.
@@ -39,128 +38,26 @@ func NewNode(genesis *Block, genesisState State, sk bls.SecretKey, net *Networki
 		sk:          sk,
 		roundInfo:   NewRoundInfo(seed),
 		chain:       NewChain(genesis, genesisState),
-		nodeIDToPK:  make(map[int]bls.PublicKey),
-		addrToPK:    make(map[Addr]bls.PublicKey),
 		memberships: make(map[bls.PublicKey]membership),
-		idToGroup:   make(map[int]*Group),
 	}
 
-	err := n.applySysTxns(genesis.SysTxns, true)
-	if err != nil {
-		// the sys txns are from the genesis block, should not
-		// happen
-		panic(err)
+	sysState := NewSysState()
+	t := sysState.Transition()
+	for _, txn := range genesis.SysTxns {
+		valid := t.Record(txn)
+		if !valid {
+			panic("sys txn in genesis is invalid")
+		}
 	}
+
+	sysState = t.Apply()
+	sysState.Finalized()
+	n.chain.LastFinalizedSysState = sysState
+	n.roundInfo.groups = sysState.groups
 
 	// advance from the 0th round to the 1st round
 	n.roundInfo.Advance(Hash(seed))
 	return n
-}
-
-type membership struct {
-	skShare bls.SecretKey
-	g       *Group
-}
-
-func (n *Node) applyReadyJoinGroup(t ReadyJoinGroupTxn) error {
-	var pk bls.PublicKey
-	err := pk.Deserialize(t.PK)
-	if err != nil {
-		return err
-	}
-
-	addr := hash(pk.Serialize()).Addr()
-	n.nodeIDToPK[t.ID] = pk
-	n.addrToPK[addr] = pk
-	return nil
-}
-
-func (n *Node) applyRegGroup(t RegGroupTxn) error {
-	var pk bls.PublicKey
-	err := pk.Deserialize(t.PK)
-	if err != nil {
-		return err
-	}
-
-	g := NewGroup(pk)
-	for _, id := range t.MemberIDs {
-		pk, ok := n.nodeIDToPK[id]
-		if !ok {
-			return errors.New("node not found")
-		}
-
-		addr := hash(pk.Serialize()).Addr()
-		g.MemberPK[addr] = pk
-	}
-
-	// TODO: parse vvec
-
-	n.idToGroup[t.ID] = g
-	return nil
-}
-
-func (n *Node) applyListGroups(t ListGroupsTxn) error {
-	gs := make([]*Group, len(t.GroupIDs))
-	for i, id := range t.GroupIDs {
-		g, ok := n.idToGroup[id]
-		if !ok {
-			return errors.New("group not found")
-		}
-		gs[i] = g
-	}
-	n.roundInfo.groups = gs
-	return nil
-}
-
-// nolint: gocyclo
-func (n *Node) applySysTxns(txns []SysTxn, noCheck bool) error {
-	// TODO: apply sys txn should be atomic. E.g., when error
-	// happens in the middle, nothing will be changed.
-	for _, txn := range txns {
-		if !noCheck {
-			// TODO: check signature, endorsement proof,
-			// etc.
-		}
-
-		dec := gob.NewDecoder(bytes.NewReader(txn.Data))
-		switch txn.Type {
-		case ReadyJoinGroup:
-			var t ReadyJoinGroupTxn
-			err := dec.Decode(&t)
-			if err != nil {
-				return err
-			}
-
-			err = n.applyReadyJoinGroup(t)
-			if err != nil {
-				return err
-			}
-		case RegGroup:
-			var t RegGroupTxn
-			err := dec.Decode(&t)
-			if err != nil {
-				return err
-			}
-
-			err = n.applyRegGroup(t)
-			if err != nil {
-				return err
-			}
-		case ListGroups:
-			var t ListGroupsTxn
-			err := dec.Decode(&t)
-			if err != nil {
-				return err
-			}
-
-			err = n.applyListGroups(t)
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
 }
 
 func (n *Node) recvBlockProposal(b *BlockProposal) {
--- pkg/consensus/sys_state.go
+++ pkg/consensus/sys_state.go
@@ -1,32 +1,176 @@
 package consensus
 
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+
+	"github.com/dfinity/go-dfinity-crypto/bls"
+)
+
 // SysState is the system state, the system state can be changed by
 // the SysTxn of each block.
 type SysState struct {
+	nodeIDToPK map[int]bls.PublicKey
+	addrToPK   map[Addr]bls.PublicKey
+	idToGroup  map[int]*Group
+	groups     []*Group
 }
 
-// Transition returns the system state transition
-func (s *SysState) Transition() *SysTransition {
-	return &SysTransition{}
+// NewSysState creates a new system state.
+func NewSysState() *SysState {
+	return &SysState{
+		nodeIDToPK: make(map[int]bls.PublicKey),
+		addrToPK:   make(map[Addr]bls.PublicKey),
+		idToGroup:  make(map[int]*Group),
+	}
 }
 
 // SysTransition is the system transition used to change the system
 // state.
 type SysTransition struct {
+	s    *SysState
+	txns []SysTxn
 }
 
 // Record records the transaction as part of the transition.
-func (s SysTransition) Record(txn SysTxn) bool {
+func (s *SysTransition) Record(txn SysTxn) bool {
+	s.txns = append(s.txns, txn)
 	return true
 }
 
 // Txns returns the recorded transactions.
 func (s *SysTransition) Txns() []SysTxn {
-	return nil
+	return s.txns
 }
 
 // Apply applies the recorded transactions and creates a new system
 // state.
 func (s *SysTransition) Apply() *SysState {
+	// TODO: this is assuming that there will be no more sys txn
+	// after genesis. This is not true after we support open
+	// participation though DKG.
+	err := s.s.applySysTxns(s.txns)
+	if err != nil {
+		// TODO: handle error when open participation is
+		// supported.
+		panic(err)
+	}
+
+	return s.s
+}
+
+// Clear clears the recorded transactions.
+func (s *SysTransition) Clear() *SysState {
+	return nil
+}
+
+// Transition returns the system state transition
+func (s *SysState) Transition() *SysTransition {
+	return &SysTransition{s: s}
+}
+
+// Finalized marks the system state as finalized.
+//
+// System state references its parent state, only records the diff
+// state, to save memory. All finalized system state execept the last
+// one will never be used, so they can be squashed into the last
+// finalized system state.
+func (s *SysState) Finalized() {
+	// TODO
+}
+
+func (s *SysState) applyReadyJoinGroup(t ReadyJoinGroupTxn) error {
+	var pk bls.PublicKey
+	err := pk.Deserialize(t.PK)
+	if err != nil {
+		return err
+	}
+
+	addr := hash(pk.Serialize()).Addr()
+	s.nodeIDToPK[t.ID] = pk
+	s.addrToPK[addr] = pk
+	return nil
+}
+
+func (s *SysState) applyRegGroup(t RegGroupTxn) error {
+	var pk bls.PublicKey
+	err := pk.Deserialize(t.PK)
+	if err != nil {
+		return err
+	}
+
+	g := NewGroup(pk)
+	for _, id := range t.MemberIDs {
+		pk, ok := s.nodeIDToPK[id]
+		if !ok {
+			return errors.New("node not found")
+		}
+
+		addr := hash(pk.Serialize()).Addr()
+		g.MemberPK[addr] = pk
+	}
+
+	// TODO: parse vvec
+
+	s.idToGroup[t.ID] = g
+	return nil
+}
+
+func (s *SysState) applyListGroups(t ListGroupsTxn) error {
+	gs := make([]*Group, len(t.GroupIDs))
+	for i, id := range t.GroupIDs {
+		g, ok := s.idToGroup[id]
+		if !ok {
+			return errors.New("group not found")
+		}
+		gs[i] = g
+	}
+	s.groups = gs
+	return nil
+}
+
+// nolint: gocyclo
+func (s *SysState) applySysTxns(txns []SysTxn) error {
+	for _, txn := range txns {
+		// TODO: check signature, endorsement proof, etc.
+		dec := gob.NewDecoder(bytes.NewReader(txn.Data))
+		switch txn.Type {
+		case ReadyJoinGroup:
+			var t ReadyJoinGroupTxn
+			err := dec.Decode(&t)
+			if err != nil {
+				return err
+			}
+
+			err = s.applyReadyJoinGroup(t)
+			if err != nil {
+				return err
+			}
+		case RegGroup:
+			var t RegGroupTxn
+			err := dec.Decode(&t)
+			if err != nil {
+				return err
+			}
+
+			err = s.applyRegGroup(t)
+			if err != nil {
+				return err
+			}
+		case ListGroups:
+			var t ListGroupsTxn
+			err := dec.Decode(&t)
+			if err != nil {
+				return err
+			}
+
+			err = s.applyListGroups(t)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
 }
