bab350770f2b76de1fad5ace6fb03d3f716a5787
Helin Wang <ustc.harry@gmail.com>
2018-06-09 19:58:25 -0700

    fix build

--- cmd/node/node.go
+++ cmd/node/node.go
@@ -39,7 +39,7 @@ func createNode(c consensus.NodeCredentials, genesis *consensus.Block, nativeCoi
 
 	db := trie.NewDatabase(ethdb.NewMemDatabase())
 	state := dex.NewState(db)
-	state = state.IssueNativeToken(&nativeCoinOwnerPK).(*dex.State)
+	state = state.IssueNativeToken(nativeCoinOwnerPK).(*dex.State)
 	return consensus.MakeNode(c, &network.Network{}, cfg, genesis, state, dex.NewTxnPool(state), u)
 }
 
